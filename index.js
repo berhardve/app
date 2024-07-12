95552a13845cad7b8ebc90f7eb1c8270f41aa378
Bernardo <bernardo.wbsystem@gmail.com>
2024-07-12 02:36:44 -0400

    d

--- index.js
+++ index.js
@@ -320,7 +320,7 @@ app.post('/raffles/:raffleId/register-winner', (req, res) => {
 });
 
 // Iniciar el servidor en el puerto 3000
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
