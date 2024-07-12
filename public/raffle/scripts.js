3881ccce1adf83d810a0b1f3f94e1633db5b18cb
Bernardo <bernardo.wbsystem@gmail.com>
2024-07-12 00:07:54 -0400

    Adelanto

--- public/raffle/scripts.js
+++ public/raffle/scripts.js
@@ -1,121 +1,162 @@
 $(document).ready(function() {
     // Cargar las rifas disponibles al cargar la página
-    $.ajax({
-        url: '/raffles',
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            $('#raffles-list').empty();
-            data.forEach(function(raffle) {
-                var raffleId = raffle.id;
-                var raffleNumbers = raffle.numbers.map(function(number) {
-                    return number.number;
-                }).join(', ');
+    fetchRaffles();
 
-                var raffleItem = $('<li>')
-                    .addClass('list-group-item raffle-item')
-                    .attr('data-raffle-id', raffleId)
-                    .html('<h5 class="raffle-title">Rifa ' + raffleId + '</h5><div class="numbers-list">' + raffleNumbers + '</div>');
-
-                $('#raffles-list').append(raffleItem);
-            });
-
-            $('#raffle-id').empty().append('<option value="">Seleccione la rifa...</option>');
-            data.forEach(function(raffle) {
-                $('#raffle-id').append('<option value="' + raffle.id + '">Rifa ' + raffle.id + '</option>');
-            });
-        },
-        error: function(err) {
-            console.error('Error al cargar las rifas:', err);
-        }
+    // Configurar eventos
+    $(document).on('click', '.raffle-item', function() {
+        var raffleId = $(this).data('raffle-id');
+        $('#raffle-id').val(raffleId);
+        $('#buy-raffle-form').slideDown();
     });
 
-        $(document).on('click', '.raffle-item', function() {
-            var raffleId = $(this).data('raffle-id');
-            $('#raffle-id').val(raffleId);
-            $('#buy-raffle-form').slideDown();
-        });
+    $('#create-raffle-form').submit(function(event) {
+        event.preventDefault();
+        var numCount = parseInt($('#numbers').val());
+        var maxAmountPerNumber = $('#max-amount-per-number').val();
 
-        $('#create-raffle-form').submit(function(event) {
-            event.preventDefault();
-            
-            var numCount = parseInt($('#numbers').val());
-            var maxAmountPerNumber = $('#max-amount-per-number').val();
-            
-            console.log('Cantidad Numeros Rifa:', numCount);
-            console.log('Monto Por Numero:', maxAmountPerNumber);
-
-            $.ajax({
-                url: '/raffles',
-                method: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({ numbers: numCount, maxAmountPerNumber: maxAmountPerNumber }),
-                success: function(data) {
-                    console.log('Rifa creada:', data);
-                    location.reload();
-                },
-                error: function(err) {
-                    console.error('Error al crear la rifa:', err);
-                    if (err.responseJSON && err.responseJSON.error) {
-                        console.error('Mensaje de error del servidor:', err.responseJSON.error);
-                    }
+        $.ajax({
+            url: '/raffles',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ numbers: numCount, maxAmountPerNumber: maxAmountPerNumber }),
+            success: function(data) {
+                console.log('Rifa creada:', data);
+                location.reload();
+            },
+            error: function(err) {
+                console.error('Error al crear la rifa:', err);
+                if (err.responseJSON && err.responseJSON.error) {
+                    console.error('Mensaje de error del servidor:', err.responseJSON.error);
                 }
-            });
+            }
         });
+    });
 
-        $('#buy-raffle-form').submit(function(event) {
-            event.preventDefault();
+    $('#buy-raffle-form').submit(function(event) {
+        event.preventDefault();
+        var raffleId = $('#raffle-id').val();
+        var number = $('#raffle-number').val();
+        var amountToBuy = $('#amount-to-buy').val();
+        var buyerName = $('#buyerName').val();
 
-            var raffleId = $('#raffle-id').val();
-            var number = $('#raffle-number').val();
-            var amountToBuy = $('#amount-to-buy').val();
-            var buyerName = $('#buyerName').val();
+        $.ajax({
+            url: '/raffles/' + raffleId + '/buy',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ buyerName: buyerName, number: number, amountToBuy: amountToBuy }),
+            success: function(data) {
+                console.log('Número de rifa comprado:', data);
+                $('#remaining-amount').val(data.remainingAmount - amountToBuy);
+                $('#error-message').text('');
+            },
+            error: function(err) {
+                console.error('Error al comprar el número de rifa:', err);
+                if (err.responseJSON && err.responseJSON.error) {
+                    $('#error-message').text(err.responseJSON.error);
+                }
+            }
+        });
+    });
 
+    // Función para registrar un ganador
+    $('#register-winner-form').submit(function(event) {
+        event.preventDefault();
+        var raffleId = $('#raffle-select-winner').val();
+        var winningNumber = $('#winning-number').val();
+
+        $.ajax({
+            url: '/raffles/' + raffleId + '/winners',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ number: winningNumber }),
+            success: function(data) {
+                console.log('Ganador registrado:', data);
+                // Aquí podrías actualizar la interfaz si es necesario, como mostrar un mensaje de éxito.
+            },
+            error: function(err) {
+                console.error('Error al registrar ganador:', err);
+                if (err.responseJSON && err.responseJSON.error) {
+                    console.error('Mensaje de error del servidor:', err.responseJSON.error);
+                }
+            }
+        });
+    });
+
+   // Función para mostrar el listado de ganadores
+    $('#show-winners-btn').click(function() {
+        var raffleId = $('#raffle-select-winner').val();
+
+        $.ajax({
+            url: '/raffles/' + raffleId + '/winners',
+            method: 'GET',
+            success: function(data) {
+                console.log('Listado de ganadores:', data);
+
+                // Limpiamos la lista actual de ganadores
+                $('#winners-list').empty();
+
+                // Mostramos los ganadores en la lista
+                data.forEach(function(winner) {
+                    var listItem = `<li class="list-group-item"> Rifa: ${winner.raffleId} <br/> Nombre: ${winner.buyerName} <br/> Monto comprado: ${winner.amountBought}  </li>`;
+                    $('#winners-list').append(listItem);
+                });
+            },
+            error: function(err) {
+                console.error('Error al obtener listado de ganadores:', err);
+                $('#winners-list').empty().append('<li class="list-group-item">Error al cargar ganadores</li>');
+            }
+        });
+    });
+
+    // Función para cargar las rifas desde el servidor
+    function fetchRaffles() {
+        fetch('/raffles')
+          .then(response => response.json())
+          .then(raffles => {
+            const raffleSelect = document.getElementById('raffle-id');
+            const raffleSelectWinner = document.getElementById('raffle-select-winner');
+
+            raffleSelect.innerHTML = '';
+            raffleSelectWinner.innerHTML = '';
+
+            raffles.forEach(raffle => {
+              const option = document.createElement('option');
+              option.value = raffle.id;
+              option.textContent = `Rifa ${raffle.id}`;
+              raffleSelect.appendChild(option);
+
+              const optionWinner = document.createElement('option');
+              optionWinner.value = raffle.id;
+              optionWinner.textContent = `Rifa ${raffle.id}`;
+              raffleSelectWinner.appendChild(optionWinner);
+            });
+          })
+          .catch(error => {
+            console.error('Error al obtener rifas:', error);
+          });
+    }
+
+    // Agregar evento para cambio de valor en el campo #raffle-number
+    $('#raffle-number').change(function() {
+        var raffleId = $('#raffle-id').val();
+        var number = $(this).val();
+
+        if (raffleId && number) {
             $.ajax({
-                url: '/raffles/' + raffleId + '/buy',
-                method: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({ buyerName: buyerName, number: number, amountToBuy: amountToBuy }),
+                url: '/raffles/' + raffleId + '/number/' + number + '/remaining',
+                method: 'GET',
                 success: function(data) {
-                    console.log('Número de rifa comprado:', data);
-                    $('#remaining-amount').val(data.remainingAmount - amountToBuy);
+                    $('#remaining-amount').val(data.remainingAmount);
                     $('#error-message').text('');
                 },
                 error: function(err) {
-                    console.error('Error al comprar el número de rifa:', err);
+                    console.error('Error al obtener la cantidad restante:', err);
+                    $('#remaining-amount').val('');
                     if (err.responseJSON && err.responseJSON.error) {
                         $('#error-message').text(err.responseJSON.error);
                     }
                 }
             });
-        });
-
-         // Agregar evento para cambio de valor en el campo #raffle-number
-    $('#raffle-number').change(function() {
-      var raffleId = $('#raffle-id').val();
-      var number = $(this).val();
-      
-      if (raffleId && number) {
-          $.ajax({
-              url: '/raffles/' + raffleId + '/number/' + number + '/remaining',
-              method: 'GET',
-              success: function(data) {
-                  $('#remaining-amount').val(data.remainingAmount);
-                  $('#error-message').text('');
-              },
-              error: function(err) {
-                  console.error('Error al obtener la cantidad restante:', err);
-                  $('#remaining-amount').val('');
-                  if (err.responseJSON && err.responseJSON.error) {
-                      $('#error-message').text(err.responseJSON.error);
-                  }
-              }
-          });
-      }
-  });
-
-  $('#raffle-number').change(function() {
-      $('#remaining-amount').val('');
-      $('#error-message').text('');
-  });
+        }
+    });
 });
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -25,8 +24,8 @@ sequelize.authenticate()
 
 // Definir modelos
 const Raffle = sequelize.define('raffle', {
-  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-  // Puedes agregar más campos según sea necesario
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true }
+  // Agrega más campos según sea necesario
 });
 
 const RaffleNumber = sequelize.define('raffle_number', {
@@ -37,26 +36,22 @@ const RaffleNumber = sequelize.define('raffle_number', {
   amount_sold: { type: Sequelize.INTEGER, defaultValue: 0 }
 });
 
-
-// Mantener el modelo Winner
 const Winner = sequelize.define('winner', {
-    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    raffleId: Sequelize.INTEGER,
-    raffleNumberId: Sequelize.INTEGER,
-    buyerName: Sequelize.STRING,
-    amountBought: Sequelize.INTEGER,
-    remainingAmount: Sequelize.INTEGER
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  raffleId: Sequelize.INTEGER,
+  raffleNumberId: Sequelize.INTEGER,
+  buyerName: Sequelize.STRING,
+  amountBought: Sequelize.INTEGER,
+  remainingAmount: Sequelize.INTEGER
 }, {
-    timestamps: false  // Deshabilitar la gestión automática de createdAt y updatedAt
+  timestamps: false  // Deshabilitar la gestión automática de createdAt y updatedAt
 });
-
 
 // Relaciones entre modelos
 Raffle.hasMany(RaffleNumber);
-RaffleNumber.belongsTo(Raffle);;
+RaffleNumber.belongsTo(Raffle);
 Winner.belongsTo(Raffle, { foreignKey: 'raffleId' });
 Winner.belongsTo(RaffleNumber, { foreignKey: 'raffleNumberId' });
-
 
 // Sincronización de modelos con la base de datos
 sequelize.sync({ force: false }) // Cambiar a true para reiniciar las tablas en cada reinicio
@@ -72,158 +67,260 @@ app.use(express.static(__dirname + '/public/raffle'));
 
 // Crear una nueva rifa
 app.post('/raffles', (req, res) => {
-    const { numbers, maxAmountPerNumber } = req.body;
-  
-    // Crear la rifa inicialmente sin los números de rifa asociados
-    Raffle.create({})
-      .then(raffle => {
-        const raffleId = raffle.id;
-        const raffleNumbers = [];
-  
-        // Generar números de rifa desde 1 hasta numbers
-        for (let i = 1; i <= numbers; i++) {
-          raffleNumbers.push({
-            raffleId: raffleId,
-            number: i,
-            max_amount: maxAmountPerNumber,
-            amount_sold: 0  // Saldo inicial para este número
-          });
-        }
-  
-        // Insertar todos los números de rifa en la base de datos
-        RaffleNumber.bulkCreate(raffleNumbers)
-          .then(() => {
-            // Devolver la rifa creada con los números de rifa asociados
-            Raffle.findByPk(raffleId, { include: [RaffleNumber] })
-              .then(raffleWithNumbers => {
-                res.json(raffleWithNumbers);
-              })
-              .catch(err => {
-                console.error('Error al obtener la rifa con números de rifa:', err);
-                res.status(500).json({ error: 'Error al obtener la rifa con números de rifa' });
-              });
-          })
-          .catch(err => {
-            console.error('Error al crear números de rifa:', err);
-            res.status(500).json({ error: 'Error al crear números de rifa' });
-          });
-      })
-      .catch(err => {
-        console.error('Error al crear la rifa:', err);
-        res.status(500).json({ error: 'Error al crear la rifa' });
-      });
-  }); 
-   
+  const { numbers, maxAmountPerNumber } = req.body;
+
+  // Crear la rifa inicialmente sin los números de rifa asociados
+  Raffle.create({})
+    .then(raffle => {
+      const raffleId = raffle.id;
+      const raffleNumbers = [];
+
+      // Generar números de rifa desde 1 hasta numbers
+      for (let i = 1; i <= numbers; i++) {
+        raffleNumbers.push({
+          raffleId: raffleId,
+          number: i,
+          max_amount: maxAmountPerNumber,
+          amount_sold: 0  // Saldo inicial para este número
+        });
+      }
+
+      // Insertar todos los números de rifa en la base de datos
+      RaffleNumber.bulkCreate(raffleNumbers)
+        .then(() => {
+          // Devolver la rifa creada con los números de rifa asociados
+          Raffle.findByPk(raffleId, { include: [RaffleNumber] })
+            .then(raffleWithNumbers => {
+              res.json(raffleWithNumbers);
+            })
+            .catch(err => {
+              console.error('Error al obtener la rifa con números de rifa:', err);
+              res.status(500).json({ error: 'Error al obtener la rifa con números de rifa' });
+            });
+        })
+        .catch(err => {
+          console.error('Error al crear números de rifa:', err);
+          res.status(500).json({ error: 'Error al crear números de rifa' });
+        });
+    })
+    .catch(err => {
+      console.error('Error al crear la rifa:', err);
+      res.status(500).json({ error: 'Error al crear la rifa' });
+    });
+});
 
 // Obtener todas las rifas con números de rifa asociados y su información adicional
 app.get('/raffles', (req, res) => {
-    Raffle.findAll({
-        include: [{
-            model: RaffleNumber
-        }]
-    })
+  Raffle.findAll({
+    include: [{
+      model: RaffleNumber
+    }]
+  })
     .then(raffles => {
-        // Formatear la respuesta para incluir la información de cantidad restante por número
-        const formattedRaffles = raffles.map(raffle => {
-            const formattedNumbers = raffle.raffle_numbers.map(number => ({
-                id: number.id,
-                raffleId: number.raffleId,
-                number: number.number,
-                max_amount: number.max_amount,
-                amount_sold: number.amount_sold,
-                remaining_amount: number.max_amount - number.amount_sold // Calculamos el monto restante
-            }));
+      // Formatear la respuesta para incluir la información de cantidad restante por número
+      const formattedRaffles = raffles.map(raffle => {
+        const formattedNumbers = raffle.raffle_numbers.map(number => ({
+          id: number.id,
+          raffleId: number.raffleId,
+          number: number.number,
+          max_amount: number.max_amount,
+          amount_sold: number.amount_sold,
+          remaining_amount: number.max_amount - number.amount_sold // Calculamos el monto restante
+        }));
 
-            return {
-                id: raffle.id,
-                numbers: formattedNumbers
-            };
-        });
+        return {
+          id: raffle.id,
+          numbers: formattedNumbers
+        };
+      });
 
-        res.json(formattedRaffles);
+      res.json(formattedRaffles);
     })
     .catch(err => {
-        console.error('Error al obtener las rifas:', err);
-        res.status(500).json({ error: 'Error al obtener las rifas' });
+      console.error('Error al obtener las rifas:', err);
+      res.status(500).json({ error: 'Error al obtener las rifas' });
     });
 });
-
-  
 
 // Comprar un número de rifa
 app.post('/raffles/:raffleId/buy', (req, res) => {
-    const { raffleId } = req.params;
-    const { buyerName, number, amountToBuy } = req.body;
+  const { raffleId } = req.params;
+  const { buyerName, number, amountToBuy } = req.body;
 
-    RaffleNumber.findOne({
-        where: { raffleId, number }
-    }).then(raffleNumber => {
-        if (!raffleNumber) {
-            return res.status(404).json({ error: 'Número de rifa no encontrado' });
-        }
+  RaffleNumber.findOne({
+    where: { raffleId, number }
+  }).then(raffleNumber => {
+    if (!raffleNumber) {
+      return res.status(404).json({ error: 'Número de rifa no encontrado' });
+    }
 
-        if (raffleNumber.amount_sold >= raffleNumber.max_amount) {
-            return res.status(400).json({ error: 'Se ha alcanzado el límite de venta para este número' });
-        }
+    if (raffleNumber.amount_sold >= raffleNumber.max_amount) {
+      return res.status(400).json({ error: 'Se ha alcanzado el límite de venta para este número' });
+    }
 
-        // Verificar si hay suficiente saldo restante para comprar
-        const remainingAmount = raffleNumber.max_amount - raffleNumber.amount_sold;
-        if (amountToBuy > remainingAmount) {
-            return res.status(400).json({ error: 'No hay suficiente saldo restante para este número' });
-        }
+    // Verificar si hay suficiente saldo restante para comprar
+    const remainingAmount = raffleNumber.max_amount - raffleNumber.amount_sold;
+    if (amountToBuy > remainingAmount) {
+      return res.status(400).json({ error: 'No hay suficiente saldo restante para este número' });
+    }
 
-        // Registrar al ganador
-        Winner.create({
-            raffleId,
-            raffleNumberId: raffleNumber.id,
-            buyerName,
-            amountBought: amountToBuy,
-            remainingAmount
-        }).then(() => {
-            // Actualizar la cantidad vendida
-            raffleNumber.amount_sold += parseInt(amountToBuy);
-            raffleNumber.save();
+    // Registrar al ganador
+    Winner.create({
+      raffleId,
+      raffleNumberId: raffleNumber.id,
+      buyerName,
+      amountBought: amountToBuy,
+      remainingAmount
+    }).then(() => {
+      // Actualizar la cantidad vendida
+      raffleNumber.amount_sold += parseInt(amountToBuy);
+      raffleNumber.save();
 
-            res.json({
-                number: raffleNumber.number,
-                buyerName: buyerName,
-                amount: amountToBuy,
-                remainingAmount: remainingAmount
-            });
-        }).catch(err => {
-            console.error('Error al registrar al ganador:', err);
-            res.status(500).json({ error: 'Error al registrar al ganador' });
-        });
-
+      res.json({
+        number: raffleNumber.number,
+        buyerName: buyerName,
+        amount: amountToBuy,
+        remainingAmount: remainingAmount
+      });
     }).catch(err => {
-        console.error('Error al comprar el número de rifa:', err);
-        res.status(500).json({ error: 'Error al comprar el número de rifa' });
+      console.error('Error al registrar al ganador:', err);
+      res.status(500).json({ error: 'Error al registrar al ganador' });
     });
-});
 
+  }).catch(err => {
+    console.error('Error al comprar el número de rifa:', err);
+    res.status(500).json({ error: 'Error al comprar el número de rifa' });
+  });
+});
 
 // Obtener la cantidad restante para un número específico de una rifa
 app.get('/raffles/:raffleId/number/:number/remaining', (req, res) => {
-    const { raffleId, number } = req.params;
+  const { raffleId, number } = req.params;
 
-    RaffleNumber.findOne({ where: { raffleId: raffleId, number: number } })
-        .then(raffleNumber => {
-            if (!raffleNumber) {
-                return res.status(404).json({ error: 'Número de rifa no encontrado' });
-            }
-            const remainingAmount = raffleNumber.max_amount - raffleNumber.amount_sold;
-            res.json({ remainingAmount: remainingAmount });
-        })
-        .catch(err => {
-            console.error('Error al obtener la cantidad restante:', err);
-            res.status(500).json({ error: 'Error al obtener la cantidad restante' });
-        });
+  RaffleNumber.findOne({ where: { raffleId: raffleId, number: number } })
+    .then(raffleNumber => {
+      if (!raffleNumber) {
+        return res.status(404).json({ error: 'Número de rifa no encontrado' });
+      }
+      const remainingAmount = raffleNumber.max_amount - raffleNumber.amount_sold;
+      res.json({ remainingAmount: remainingAmount });
+    })
+    .catch(err => {
+      console.error('Error al obtener la cantidad restante:', err);
+      res.status(500).json({ error: 'Error al obtener la cantidad restante' });
+    });
 });
 
+// Obtener los ganadores de una rifa específica (GET)
+app.get('/raffles/:raffleId/winners', (req, res) => {
+  const { raffleId } = req.params;
 
+  Winner.findAll({
+    where: { raffleId },
+    include: [{
+      model: RaffleNumber,
+      attributes: ['number'] // Incluir solo los atributos necesarios del modelo RaffleNumber
+    }]
+  })
+    .then(winners => {
+      // Formatear la respuesta para asegurar que el número de rifa esté presente y no sea undefined
+      const formattedWinners = winners.map(winner => ({
+        id: winner.id,
+        raffleId: winner.raffleId,
+        raffleNumberId: winner.raffleNumberId,
+        buyerName: winner.buyerName,
+        amountBought: winner.amountBought,
+        remainingAmount: winner.remainingAmount,
+        number: winner.raffle_number ? winner.raffle_number.number : 'Número no especificado'
+      }));
 
-// Escuchar en un puerto específico
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+      res.json(formattedWinners);
+    })
+    .catch(err => {
+      console.error('Error al obtener ganadores:', err);
+      res.status(500).json({ error: 'Error al obtener ganadores' });
+    });
+});
+
+// Registrar un ganador para una rifa y un número específico (POST)
+app.post('/raffles/:raffleId/winners', (req, res) => {
+  const { raffleId } = req.params;
+  const { number, buyerName, amountBought, remainingAmount } = req.body;
+
+  RaffleNumber.findOne({
+    where: { raffleId, number }
+  })
+    .then(raffleNumber => {
+      if (!raffleNumber) {
+        return res.status(404).json({ error: 'Número de rifa no encontrado' });
+      }
+
+      // Crear un nuevo registro de ganador
+      Winner.create({
+        raffleId,
+        raffleNumberId: raffleNumber.id,
+        buyerName,
+        amountBought,
+        remainingAmount
+      })
+        .then(newWinner => {
+          res.json(newWinner);
+        })
+        .catch(err => {
+          console.error('Error al registrar ganador:', err);
+          res.status(500).json({ error: 'Error al registrar ganador' });
+        });
+    })
+    .catch(err => {
+      console.error('Error al buscar número de rifa:', err);
+      res.status(500).json({ error: 'Error al buscar número de rifa' });
+    });
+});
+
+// Seleccionar un ganador para una rifa y un número específico
+app.post('/raffles/:raffleId/register-winner', (req, res) => {
+  const { raffleId } = req.params;
+  const { number } = req.body;
+
+  RaffleNumber.findOne({
+    where: { raffleId, number }
+  })
+    .then(raffleNumber => {
+      if (!raffleNumber) {
+        return res.status(404).json({ error: 'Número de rifa no encontrado' });
+      }
+
+      // Encontrar al comprador de este número de rifa y registrar como ganador
+      Winner.findOne({
+        where: { raffleNumberId: raffleNumber.id }
+      })
+        .then(winner => {
+          if (!winner) {
+            return res.status(404).json({ error: 'No se encontró un ganador para este número de rifa' });
+          }
+
+          // Devolver la información del ganador
+          res.json({
+            raffleId: winner.raffleId,
+            raffleNumberId: winner.raffleNumberId,
+            buyerName: winner.buyerName,
+            amountBought: winner.amountBought,
+            remainingAmount: winner.remainingAmount
+          });
+        })
+        .catch(err => {
+          console.error('Error al buscar al ganador:', err);
+          res.status(500).json({ error: 'Error al buscar al ganador' });
+        });
+    })
+    .catch(err => {
+      console.error('Error al buscar el número de rifa:', err);
+      res.status(500).json({ error: 'Error al buscar el número de rifa' });
+    });
+});
+
+// Iniciar el servidor en el puerto 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
